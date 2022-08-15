13f841c69d5bd1bcb8ed5fd69edc6639f80586be
Anton <rjhrust@hotmail.com>
2022-08-15 22:37:15 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 //тут будет таймер
+function startTime(){
+ var startDate = new Date(2022,7,15,0,0,0,0);
+ var curentDate = Date.now();
+}
+function dif(f){
+var daysdif = curentDate - startDate;
+return daysdif;
+}
+window.onload = function
